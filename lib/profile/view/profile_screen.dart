b0f9273f4ccc0d9c3f89133d91a646ec8eb00fa4
Amit Choudhary <choudharyamit2255@gmail.com>
2024-12-21 19:16:47 +0530

    profile

--- lib/profile/view/profile_screen.dart
+++ lib/profile/view/profile_screen.dart
@@ -3,33 +3,51 @@ import 'package:provider/provider.dart';
 import 'package:shoping_hub_admin/auth/provider/auth_provider.dart';
 import 'package:shoping_hub_admin/auth/view/login_screen.dart';
 import 'package:shoping_hub_admin/core/app_util.dart';
+import 'package:shoping_hub_admin/order/view/order_screen.dart';
 
-class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({super.key});
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({ super.key});
 
-  @override
-  State<ProfileScreen> createState() => _ProfileScreenState();
-}
 
-class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Profile Screen"),),
-      body: Center(child:
-        IconButton(onPressed: (){
-          logout(context);
+      appBar: AppBar(title: const Text("Profile Screen"),),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              IconButton(onPressed: (){
+                logout(context);
 
-        },icon: Icon(Icons.logout),),),
+              },icon: const Icon(Icons.logout),),Text("logout"),
+            ],
+          ),
+          Row(
+            children: [
+              IconButton(onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context){
+                  return OrderScreen( );
+                }));
+
+              },icon: const Icon(Icons.shopping_bag_outlined),),Text("orders")
+            ],
+          ),
+
+        ],
+      ),
     );
-  }Future logout(BuildContext context)async{
+  }
+
+Future logout(BuildContext context)async{
     final provider = Provider.of<AuthProvider>(context,listen: false);
     await provider.logout();
 
-
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>
+   if(context.mounted) {
+     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>
          LoginScreen(),
 
-      ));AppUtil.showToast("logout succesfully");
-    }}
+     ));
+     AppUtil.showToast("logout Successfully");
+   }    }}
 
